27822b615f43ff64040c7069e1b5531d61300b73
Nick Cellino <nacellin@buffalo.edu>
2014-12-28 21:34:33 -0500

    added pyc files to ignore. added mongo installation to bootstrap.sh. realized vagrant setup is pointless

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ apt-get update
 apt-get install -y python-pip vim
 
 pip install -r /vagrant/requirements.txt
+
+
+# Mongo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update
+apt-get install -y mongodb-org
